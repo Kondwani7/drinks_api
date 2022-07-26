9e1e256007d2c773642e1ff4fdb146a578b36dc6
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-26 22:00:56 +0200

    POST method

--- server/views.py
+++ server/views.py
@@ -2,19 +2,35 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+#CRUD operations
+@api_view(['GET', 'POST'])
 #get data from drinks table
 def drinksList(request):
     """
     Description
         - this function will get the drinks from our table in json format
     Args:
-        - request: perform actions on drinks gotten
+        - request: perform CRUD actions on drinks gotten
     Returns:
         - drinks in json format
+        - updates drinks
+        - posts a new drink
+        - deletes a drink
     """
-    #get drinks
-    drinks = Drink.objects.all()
-    #serialize them
-    serializer = DrinkSerializer(drinks, many=True)
-    #return json
-    return JsonResponse({"drinks": serializer.data}, safe=False)
+    if request.method == 'GET':
+        #get drinks
+        drinks = Drink.objects.all()
+        #serialize them
+        serializer = DrinkSerializer(drinks, many=True)
+        #return json
+        return JsonResponse({"drinks": serializer.data})
+    #POST
+    #used to validate if a new file is created
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
